fab500f6139b147e3016145d9648909ad798a238
kd0171 <kido.0117.1895@gmail.com>
2023-11-24 17:47:50 +0100

    add atomic design

--- swtp17mockup/src/App.vue
+++ swtp17mockup/src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HowToTest msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msgs="this String will be sent to the component <template>"/>
 </template>
 
 <script>
+import HowToTest from './components/HowToTest.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    HowToTest,
     HelloWorld
   }
 }
